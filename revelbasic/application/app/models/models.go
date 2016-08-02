3b726896e1660b07de8e7a13390ca405739127a5
juniorojha <ojha.iiitm@gmail.com>
2016-08-02 13:49:11 +0530

    categories test 3 - models.go find.all

--- revelbasic/application/app/models/models.go
+++ revelbasic/application/app/models/models.go
@@ -17,7 +17,7 @@ func Collection(s *mgo.Session) *mgo.Collection {
 }
 
 func GetCategories(s *mgo.Session) {
-return Collection(s).Find()
+return Collection(s).Find().All()
 }
 
 /*func GetBookByObjectId(s *mgo.Session, Id bson.ObjectId) *Book {
