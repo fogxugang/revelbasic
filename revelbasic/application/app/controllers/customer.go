b51588f9c789d2be4bc7b8b529d38d6d8c6b48ee
juniorojha <ojha.iiitm@gmail.com>
2016-08-11 10:48:14 +0530

    Customer.go

---
+++ revelbasic/application/app/controllers/customer.go
@@ -0,0 +1,19 @@
+package controllers
+/*
+import (
+    "github.com/sadhanandh/revelbasic"
+   // "github.com/sadhanandh/revelbasic/application/app/models"
+    "github.com/revel/revel"
+    "crypto/hmac"
+    "crypto/sha256"
+)
+
+type Customer struct {
+    *revel.Controller
+    revelbasic.MongoController
+}
+
+func (c Customer) Verify(phone []byte, firstName string, secretKey []byte) revel.Result {
+    var mac = hmac.New(sha256.New, phone)
+    return hmac.Equal(mac, secretKey)
+}*/
